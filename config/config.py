84b3bec1f9ff93cfca333fc3a7de41507619a385
daffa <daffa@pintu.co.id>
2022-08-30 18:31:09 +0700

    test 4

--- config/config.py
+++ config/config.py
@@ -17,7 +17,7 @@ testRun = 61
 
 # SLACK
 slack_webhook = "https://hooks.slack.com/services/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"
-slack_webhook_daffa = "https://hooks.slack.com/services/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"
+slack_webhook_daffa = "https://hooks.slack.com/services/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"
 slack_title = "Slack Api Automation"
 
 
